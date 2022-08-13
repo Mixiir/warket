bf0fe1a02dcfb25610ee7966410b9e07de3efd77
Mikk Narusberg <mikk.narusberg@gmail.com>
2022-08-13 14:55:41 +0300

    Added mysql database connection

--- warket/settings.py
+++ warket/settings.py
@@ -75,9 +75,17 @@ WSGI_APPLICATION = 'warket.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'default_backup': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_IP'),
+        'PORT': '3306',
     }
 }
 
