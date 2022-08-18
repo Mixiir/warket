5c036ce009c7ab66fc6300cdf547a86039317418
Mikk Narusberg <mikk.narusberg@gmail.com>
2022-08-18 21:46:28 +0300

    Profile image size changed. Included user fk under profile model

---
+++ warket_viewer/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from users.models import Profile
+from .models import Wine
+
+
+class CreateWine(forms.ModelForm):
+    class Meta:
+        model = Wine
+        fields = ['name', 'image', 'description', 'vintage', 'alcohol_content', 'price_per_unit',
+                  'units_in_stock', 'manufacturer', 'type', 'variety', 'user']
