b1197e4885ab9be8c12793693ff427f56ab47ce8
Mikk Narusberg <mikk.narusberg@gmail.com>
2022-08-30 20:39:06 +0300

    Updated migrations

--- warket_viewer/views.py
+++ warket_viewer/views.py
@@ -58,7 +58,6 @@ class WineListView(ListView):
             )
         return wines
 
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['page_is'] = 'wines'
