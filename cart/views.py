1a14a4fbd8f495ca8179ac14e4f6b1dc439c8958
Mikk Narusberg <mikk.narusberg@gmail.com>
2022-08-31 12:34:39 +0300

    Major rework on cart view.

---
+++ cart/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.decorators.http import require_POST
+from .cart import Cart
+from .forms import CartAddProductForm
+from warket_viewer.models import Wine
+
+
+@require_POST
+def cart_add(request, wine_id):
+    cart = Cart(request)
+    wine = get_object_or_404(Wine, pk=wine_id)
+    form = CartAddProductForm(request.POST)
+    if form.is_valid():
+        cd = form.cleaned_data
+        cart.add(wine=wine, quantity=cd['quantity'], update_quantity=cd['update'])
+        return redirect('cart:cart_detail')
+
+
+def cart_remove(request, wine_id):
+    cart = Cart(request)
+    wine = get_object_or_404(Wine, pk=wine_id)
+    cart.remove(wine)
+    return redirect('cart:cart_detail')
+
+
+def cart_detail(request):
+    cart = Cart(request)
+    return render(request, 'cart/detail.html', {'cart': cart})
