74a6bfe18e52042f791ecfb4113a0a00435e4413
Mikk Narusberg <mikk.narusberg@gmail.com>
2022-09-02 18:17:23 +0300

    Fixed user profile model.

--- users/models.py
+++ users/models.py
@@ -7,7 +7,6 @@ import datetime
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default="default.jpg", upload_to="profile_pics")
-    birth_date = models.DateField("Date", default=datetime.date.today)
     bio = models.TextField(blank=True, null=True)
     location = models.CharField(max_length=100, choices=COUNTRIES, blank=True, null=True)
     language = models.CharField(max_length=100, choices=COUNTRIES, blank=True, null=True)
